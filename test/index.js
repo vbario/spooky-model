9cb5d484ed54d595e8c1d729a6f017ce46e15e46
tipasoft <tipasoft@lowerprofile.com>
2016-11-25 11:31:08 -0500

    Added testing of .get(path) to test file

--- test/index.js
+++ test/index.js
@@ -3,6 +3,6 @@ var spookyModel = require('../index');
 spookyModel.init( require('./model/boo'), 'en' );
 
 var content = spookyModel.getContent('spook');
-var content2 = spookyModel.get('spook.heading');
+var content2 = spookyModel.get('spook');
 
 console.log('content: ', content, content2);
